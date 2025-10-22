361aba2d219cf9f2c36c17a8f2f49baec4617ad4
Daniel Mateu <danielmateu86@gmail.com>
2025-10-22 16:13:29 +0200

    feat: actualizar estilos y agregar nuevos componentes de UI en la aplicación

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -11,6 +11,11 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  // variable: "--font-dm-sans",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +31,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${dmSans.className} antialiased`}
       >
         <ThemeProvider
           attribute="class"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,15 +9,20 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Progress } from "@/components/ui/progress"
+import { Textarea } from "@/components/ui/textarea"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
 
 export default function Home() {
   const { setTheme } = useTheme()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24 bg-rose-200 dark:bg-rose-700 space-y-20">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="outline">
+          <Button variant="themeToggle">
             <Sun />
             {/* Light */}
           </Button>
@@ -33,6 +38,29 @@ export default function Home() {
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+
+      <div className="flex flex-col gap-y-4">
+        <div className="">
+          <Button variant="elevated">Button</Button>
+        </div>
+        <div className="">
+          <Input placeholder="Input field" />
+        </div>
+        <div className="">
+          <Progress value={50} className="w-64" />
+        </div>
+        <div className="">
+          <Textarea placeholder="Textarea field" />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Checkbox id="checkbox" />
+          <Label htmlFor="checkbox" className="ml-2">
+            Checkbox
+          </Label>
+        </div>
+
+      </div>
     </main>
   );
 }
