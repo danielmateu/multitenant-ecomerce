1af5f0c23707e7fa41f6dd57a86fad77a3a000ee
Daniel Mateu <danielmateu86@gmail.com>
2025-10-22 16:23:20 +0200

    feat: actualizar estilos de componentes Checkbox, Input, Progress y Textarea para soporte de modo oscuro

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -16,7 +16,7 @@ function Progress({
       className={cn(
         "relative h-3 w-full overflow-hidden rounded-full",
         // Modified classes
-        "border bg-transparent",
+        "border bg-transparent dark:bg-slate-800",
         className
       )}
       {...props}
